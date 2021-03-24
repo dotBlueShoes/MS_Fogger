64c1b9b0c8f574fe976a0dadfb7c4d8fb20cea0c
Matthew <smatthewbloodson@gmail.com>
2021-03-24 18:37:52 +0100

    Clearing

--- src/main/java/dotblueshoes/fogger/Fogger.java
+++ src/main/java/dotblueshoes/fogger/Fogger.java
@@ -17,7 +17,7 @@ import dotblueshoes.fogger.event.*;
 @Mod( modid = Fogger.MODID,  version = Fogger.VERSION, useMetadata = true)
 public class Fogger {
     public static final String MODID = "fogger";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "2";
 
     @Instance(MODID)
     public static Fogger instance;
